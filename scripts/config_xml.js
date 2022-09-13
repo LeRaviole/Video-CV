a222d3f12e917846dec7ef20311fb81884cf3818
Iván Finochietti <110503793+LeRaviole@users.noreply.github.com>
2022-09-13 15:54:46 -0300

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,46 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2022-09-13 03:47:23 p. m." dc:source="Camtasia,9.1.2,enu" dc:title="CV Video" tscDM:firstFrame="CV_Video_First_Frame.png" tscDM:originId="74EB52D8-6023-42F3-AA77-538C5F0DBE67" tscDM:project="CV Video">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="65300"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="1080" stDim:w="608"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="61470" xmpDM:duration="3830" tscDM:boundingPoly="29,0;383,0;383,260;29,260;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://github.com/LeRaviole" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmp:label="2" xmpDM:startTime="61470" xmpDM:duration="3830" tscDM:boundingPoly="19,230;318,230;318,582;19,582;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://www.instagram.com/dr___zero/" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmp:label="3" xmpDM:startTime="61470" xmpDM:duration="3830" tscDM:boundingPoly="29,599;290,599;290,892;29,892;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://www.linkedin.com/in/iv%C3%A1n-finochietti-47a68a212/" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmp:label="4" xmpDM:startTime="61470" xmpDM:duration="3830" tscDM:boundingPoly="29,963;608,963;608,1079;29,1079;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="CV Video.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="CV_Video_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="CV_Video_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
